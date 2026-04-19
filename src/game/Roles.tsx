a4f6729c3ca3156347299b9cbd582469cc66013e
whatever <fuzzyetch@gmail.com>
2026-04-18 23:47:31 -0400

    Add more roles, UNDO ME

--- src/game/Roles.tsx
+++ src/game/Roles.tsx
@@ -11,11 +11,23 @@ export type Role = {
 export const ALL_ROLES: Role[] = [
     // Evil roles
     {
-        id: "serial-killer",
-        name: "Serial Killer",
+        id: "killer",
+        name: "Killer",
         type: "evil",
-        description: "To Implement",
+        description: "Once per night, the serial killer can kill a player.",
     },
+    {
+        id: "corrupter",
+        name: "Corrupter",
+        type: "evil",
+        description: "On the first night, turn a player into a killer. The killer does not know you are the corrupter.",
+    },
+    {
+        id: "stalker",
+        name: "Stalker",
+        type: "evil",
+        description: "",
+    }
     // Good roles
     {
         id: "citizen",
@@ -23,5 +35,41 @@ export const ALL_ROLES: Role[] = [
         type: "good",
         description: "To Implement",
     },
+    {
+        id: "coroner",
+        name: "Coroner",
+        type: "good",
+        description: "To Implement",
+    },
+    {
+        id: "bitter-bloom",
+        name: "Bitter Bloom",
+        type: "good",
+        description: "Once per game, the bitter bloom can bring back a dead player as a zombie. The zombie can vote, but cannot speak and has no powers at night.",
+    },
+    {
+        id: "powder-keg",
+        name: "Powder Keg",
+        type: "good",
+        description: "Once per game, the powder keg can set a trap. All players that interact with the powder keg that night are killed.",
+    },
+    {
+        id: "witch-doctor",
+        name: "Witch Doctor",
+        type: "good",
+        description: "Once per game, the witch docter can turn ANY player into a citizen.",
+    },
     // Other roles
+    {
+        id: "martyr",
+        name: "Martyr",
+        type: "other",
+        description: "At the end of the game, the martyr wins if they were killed at night.",
+    },
+    {
+        id: "zombie",
+        name: "Zombie",
+        type: "other",
+        description: "To Implement",
+    }
 ];
